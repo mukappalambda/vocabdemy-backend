39b39146ab10fed13d57aca07602205c7662487b
Kevin Liao <q85292542000@gmail.com>
2022-07-03 03:38:27 +0800

    feat(app.tests.test_vocab): add a test for the update vocab endpoint

--- app/tests/test_vocab.py
+++ app/tests/test_vocab.py
@@ -1,5 +1,6 @@
 from app import main
 from fastapi.testclient import TestClient
+from fastapi import status
 
 client = TestClient(main.app)
 
@@ -13,3 +14,14 @@ class TestVocab:
         print(response)
 
         assert response.status_code == 201
+
+    def test_update_vocab(self):
+
+        id = 1
+
+        response = client.put(
+            f"/vocabs/{id}",
+            json={"vocab": "cba"}
+        )
+
+        assert response.status_code == status.HTTP_204_NO_CONTENT
