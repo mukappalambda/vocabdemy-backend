62fdec540a134c4920d2af43dbd90a4d5e771b8e
Kevin Liao <q85292542000@gmail.com>
2023-02-27 23:17:45 +0800

    fix: add Optional to the function signature

--- app/api/utils.py
+++ app/api/utils.py
@@ -1,9 +1,10 @@
+from typing import Optional
 from fastapi import HTTPException, status
 
 from app.schemas import UserInDB
 
 
-def check_obj_or_raise_exception(obj: UserInDB):
+def check_obj_or_raise_exception(obj: Optional[UserInDB]):
     if not obj:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
