4c8a547bc2dea285aa5dba8533e4128620736d07
mukappalambda <maokailan24@gmail.com>
2023-10-29 00:26:43 +0800

    ci(tests): enable unit tests using the in-memory database
    issue #13

--- app/core/config.py
+++ app/core/config.py
@@ -5,12 +5,18 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     API_V1_STR: str = "/v1"
-    POSTGRES_SERVER = os.environ.get("POSTGRES_SERVER", "postgres")
+    POSTGRES_HOST = os.environ.get("POSTGRES_HOST", "postgres")
     POSTGRES_PORT = os.environ.get("POSTGRES_PORT", 5432)
     POSTGRES_USER = os.environ.get("POSTGRES_USER", "postgres")
     POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD", "postgres")
     POSTGRES_DB = os.environ.get("POSTGRES_DB", "demo")
-    SQLALCHEMY_DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+    POSTGRES_DB_URL: str = os.environ.get(
+        "POSTGRES_DB_URL",
+        (
+            f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}"
+            f"@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+        ),
+    )
 
 
 settings = Settings()
