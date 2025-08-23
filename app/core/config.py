6b6211fea7d9cc0307e54e142e2aead1bd3ee4d9
Mao-Kai Lan <maokailan24@gmail.com>
2025-08-23 19:49:33 +0800

    🐛 fix(config): fix pydantic's PydanticUserError
    Signed-off-by: Mao-Kai Lan <maokailan24@gmail.com>

--- app/core/config.py
+++ app/core/config.py
@@ -6,11 +6,11 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     API_V1_STR: str = "/v1"
     APP_VERSION: str = os.environ.get("APP_VERSION", "dev")
-    POSTGRES_HOST = os.environ.get("POSTGRES_HOST", "postgres")
-    POSTGRES_PORT = os.environ.get("POSTGRES_PORT", 5432)
-    POSTGRES_USER = os.environ.get("POSTGRES_USER", "postgres")
-    POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD", "postgres")
-    POSTGRES_DB = os.environ.get("POSTGRES_DB", "demo")
+    POSTGRES_HOST: str = os.environ.get("POSTGRES_HOST", "postgres")
+    POSTGRES_PORT: int = os.environ.get("POSTGRES_PORT", 5432)
+    POSTGRES_USER: str = os.environ.get("POSTGRES_USER", "postgres")
+    POSTGRES_PASSWORD: str = os.environ.get("POSTGRES_PASSWORD", "postgres")
+    POSTGRES_DB: str = os.environ.get("POSTGRES_DB", "demo")
     POSTGRES_DB_URL: str = os.environ.get(
         "POSTGRES_DB_URL",
         (
