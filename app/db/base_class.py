3cf51e54528901d7bd961acb1172d0a1ee0e3351
Kevin Liao <q85292542000@gmail.com>
2025-08-24 00:52:43 +0800

    ✨ Add mcp deletion and get all operations

--- app/db/base_class.py
+++ app/db/base_class.py
@@ -1,11 +1,15 @@
+from typing import Any, cast
 from sqlalchemy import Column, Integer
 from sqlalchemy.ext.declarative import as_declarative, declared_attr
 
 
 @as_declarative()
-class Base:
+class NewBase:
     @declared_attr
     def __tablename__(cls):
         return cls.__name__.lower()
 
     id = Column(Integer, primary_key=True)
+
+
+Base = cast(Any, NewBase)
---
+++ app/db/database.py
@@ -0,0 +1,20 @@
+from sqlalchemy.orm import sessionmaker
+from app.db.base import Base
+from app.db.session import engine
+
+
+class Database:
+    """Mock database class for example."""
+
+    @classmethod
+    async def connect(cls) -> "Database":
+        """Connect to database."""
+        Base.metadata.create_all(bind=engine)
+        return cls()
+
+    async def disconnect(self) -> None:
+        """Disconnect from database."""
+
+    def get_session_maker(self):
+        """Get a new session maker."""
+        return sessionmaker(autocommit=False, autoflush=False, bind=engine)
