16ae67acc8ce505c33b0f39986270ea3e5abf021
Mao-Kai Lan <maokailan24@gmail.com>
2025-09-07 13:08:35 +0800

    🧑‍💻 chore(config): update database related env vars
    Signed-off-by: Mao-Kai Lan <maokailan24@gmail.com>

--- app/db/session.py
+++ app/db/session.py
@@ -3,6 +3,6 @@ from sqlalchemy.orm import sessionmaker
 
 from app.core.config import settings
 
-engine = create_engine(settings.POSTGRES_DB_URL)
+engine = create_engine(settings.DB_DSN)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
