b3a690e5a47a102ba86d3fedb8b98e74f64efddb
Mao-Kai Lan <maokailan24@gmail.com>
2025-10-07 15:10:59 +0800

    🐛 fix(session): set `check_same_thread` to False (#60)
    Signed-off-by: Mao-Kai Lan <maokailan24@gmail.com>

--- app/db/session.py
+++ app/db/session.py
@@ -3,6 +3,6 @@ from sqlalchemy.orm import sessionmaker
 
 from app.core.config import settings
 
-engine = create_engine(settings.DB_DSN)
+engine = create_engine(settings.DB_DSN, connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
